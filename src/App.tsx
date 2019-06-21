83c74d0449d049610a6d4764913285b466145ad2
Nikita Chikunov <ninjaken32@gmail.com>
2019-06-21 21:55:09 +0500

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/nova-light/theme.css'
 import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 import styles from './app.module.css';
 import {Home} from "./components/home";
